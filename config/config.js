45e822172a349fc9b4bfc61c1c78c493031cf124
TrevTheCat <alicegdunn@hotmail.co.uk>
2015-12-15 12:10:49 +0000

    changed a comma

--- config/config.js
+++ config/config.js
@@ -2,6 +2,6 @@ var MongoUri = process.env.MONGOLAB_URI || 'mongodb://localhost/worldBook'
 
 
 module.exports = {
-  'secret': process.env.SECRET;
+  'secret': process.env.SECRET,
   'database' :  MongoUri
 };
