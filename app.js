f8d4aa251e1ebe22c688e8f89a1eb4a2c3272646
TrevTheCat <alicegdunn@hotmail.co.uk>
2015-12-17 11:11:19 +0000

    changed to local

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(morgan('dev'));
 app.use(cors({
-  origin: "https://worldwize.herokuapp.com" || "https://localhost:8000"
+  origin: "https://localhost:8000"
 }));
 app.use(passport.initialize());
 
