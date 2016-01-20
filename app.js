081e061de1ce8f08c4b9a74727f6ca628abd8cc2
TrevTheCat <alicegdunn@hotmail.co.uk>
2016-01-20 15:28:15 +0000

    changing for local host development

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(morgan('dev'));
 app.use(cors({
-  origin: "https://localhost:3000"
+  origin: "https://localhost:8000"
 }));
 app.use(passport.initialize());
 
